dc782458d2501d35ce1ad89e9e5baee7ff611c96
Matt98x <matteo.palmas7@gmail.com>
2025-03-11 11:24:42 +0100

    Reorganizing the repository

--- Application/Scripts/Translator.py
+++ Application/Scripts/Translator.py
@@ -520,21 +520,21 @@ class Graph:
                         temp=input_string.split("\"")[1]
                         in_string=f"{temp}".replace("&","and")
                         if in_string[-1]==" ":
-                            in_string=in_string[:-2]
+                            in_string=in_string[:-1]
                     else:
                         temp=input_string
                         in_string=f"{input_string}"
                         if in_string[-1]==" ":
-                            in_string=in_string[:-2]
+                            in_string=in_string[:-1]
                     if "\"" in output_string:
                         temp=output_string.split("\"")[1]
                         out_string=f"{temp}".replace("&","and")
                         if out_string[-1]==" ":
-                            out_string=out_string[:-2]
+                            out_string=out_string[:-1]
                     else:
                         out_string=f"{output_string}"
                         if out_string[-1]==" ":
-                            out_string=out_string[:-2]
+                            out_string=out_string[:-1]
                     print(input_string,out_string+"|")
                     new_arc=generateArc(Templateroot2,in_string,out_string,"normal",inscription)
                    
